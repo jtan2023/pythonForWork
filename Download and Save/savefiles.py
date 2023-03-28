80cc61fbc4daf0c0b395e9ee5c52e94d6820a28d
jtan2023 <122414931+jtan2023@users.noreply.github.com>
2023-03-28 16:57:24 -0400

    Update savefiles.py

--- Download and Save/savefiles.py
+++ Download and Save/savefiles.py
@@ -3,7 +3,7 @@ import requests
 from urllib.parse import unquote
 
 url = 'https://clientcentral.yardi.com/cloud_file_manager.aspx?path=h%252fVmntTUHJSv6hjyBbv0jFAKTMmTY%252fUaZOV3DSQ%252fLdtrdlcTwIb2bKAO3cPlhNxp0VGhiECLOVCRBrVvt08QyQtlGITWXzi%252fB95m41pDjw5s%252b9FIgG1p%252f5SqowAX3naQ%252fUfrSvbbPtlakGo4uruxrgONgB68%252b0utdDROgNc8xw0%253d&webID=BFQBnMgiE5YzjrzUQBrk%252fw%253d%253d'
-download_dir = 'C:/path/to/my_new_folder'
+download_dir = 'C:\Users\jtan\OneDrive - Beacon Communities LLC\Desktop\IT\IT Ticket\2023\3\move to TEST\my_new_folder'
 
 if not os.path.exists(download_dir):
     os.makedirs(download_dir)
